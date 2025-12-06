4419f2b38ac45b1e2ec18de21ca2de1939203d61
hooom1234 <tawunpingma21@gmail.com>
2025-12-07 02:51:20 +0700

    First Sun Update

--- src/data/jobsData.js
+++ src/data/jobsData.js
@@ -1,13 +1,19 @@
 export const jobs = [
-    {
+  {
     title: "2023 : The Beginning",
     time: ["Jan 2023", "Dec 2023"],
     description: "จุดเริ่มต้นเส้นทาง Coding",
     list: [
-      "Volunteer: จิตอาสาโรงเรียนและชุมชน",
-      "Learning: เริ่มต้นเรียนรู้ HTML, CSS, PHP, MySQL (จุดเริ่มต้น)",
-      "ศิลปหัตถกรรมนักเรียน ครั้งที่ 71: เหรียญทอง (ชนะเลิศ) Web Applications ",
-      "โครงงาน Arduino: Smart Toilet โดย PictoBlox และ Meachcine Learning",
+      {text:"Volunteer: จิตอาสาโรงเรียนและชุมชน", image:"https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765048519/image_12_fqx3wl.png"},
+      {text:"Learning: เริ่มต้นเรียนรู้ HTML, CSS, PHP, MySQL (จุดเริ่มต้น)",image:"https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765048940/376355236_295288639869940_1604955081092817018_n_y0gyei.png"},
+      {
+        text: "ศิลปหัตถกรรมนักเรียน ครั้งที่ 71: เหรียญทอง (ชนะเลิศ) Web Applications",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765048301/m4_ym542x.jpg" 
+      },
+      {
+        text: "โครงงาน Arduino: Smart Toilet โดย PictoBlox และ Machine Learning",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765048446/417586987_344178381855464_3405081565351513499_n_1_qoju47.png"
+      },
     ]
   },
   {
@@ -16,9 +22,18 @@ export const jobs = [
     description: "ปีแห่งการนำความรู้ด้าน Web Development มาประยุกต์ใช้ในการทำโครงงานและส่งเข้าแข่งขัน",
     list: [
       "ศึกษาด้วยตนเอง: JavaScript, React, Node.js, Game Development (Unity/C#)",
-      "ศิลปหัตถกรรมนักเรียน ครั้งที่ 72: เหรียญทอง (รองชนะเลิศอันดับ 1) Web Applications",
-      "Timeless of Lampang: หัวหน้าทีมพัฒนาเกมประวัติศาสตร์ลำปาง นำเสนอ NSC 2024 (System Design & Dev)",
-      "Thailand Cyber Top Talent 2024: เจอรายการ Cybersecurity เป็นครั้งแรกลองเข้าแข่งขัน",
+      {
+        text: "ศิลปหัตถกรรมนักเรียน ครั้งที่ 72: เหรียญทอง (รองชนะเลิศอันดับ 1) Web Applications",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765049147/A-1_f3fnp1.jpg"
+      },
+      {
+        text: "Timeless of Lampang: หัวหน้าทีมพัฒนาเกมประวัติศาสตร์ลำปาง นำเสนอ NSC 2024 (System Design & Dev)",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765049308/533850951_1175007774460502_3627828925086135077_n_n2ozmf.jpg"
+      },
+      {
+        text: "Thailand Cyber Top Talent 2024: เจอรายการ Cybersecurity เป็นครั้งแรก ลองเข้าแข่งขัน",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765049569/495372290_3019453788216485_389879238953980250_n_vctjfd.jpg"
+      },
       "เกิดความสนใจและฝึกทักษะ Cybersecurity อย่างต่อเนื่อง, Kali Linux, Exploitation",
     ]
   },
@@ -27,14 +42,34 @@ export const jobs = [
     time: ["Present", "2025"],
     description: "มุ่งเน้นการแข่งขันระดับประเทศและการเตรียมความพร้อมสู่มหาวิทยาลัย (NSC & Cyber Top Talent)",
     list: [
-      "Certificates: Cybersecurity Foundation Basic(NCSA) และอบรมการใช้งาน Supercomputer Lanta",
-      "NCSA Boot Camp: รองชนะเลิศอันดับ 2 การแข่งขัน Mini CTF ระดับภูมิภาค",
-      "CoE@PSU CTF: รางวัลชมเชย ทีม SirHackalot LKS และอันดับ 2 จากผู้เข้าแข่งขันทั้งหมด 44 คน",
-      "NSC 2025: ผ่านเข้ารอบนำเสนอผลงาน โครงงาน 'LksBrightSteps', React Native + PHP + MySQL",
-      "SMTE Northern 2025: เหรียญทอง การนำเสนอโครงงานวิชาการ",
-      "Thailand Cyber Top Talent 2025: อันดับ 56 (ประเภทเดี่ยว) จาก 1,319 คน",
+      {
+        text: "อบรมการใช้งาน Supercomputer Lanta",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765049673/image_13_ywqmhk.png"
+      },
+      {
+        text: "Certificates: Cybersecurity Foundation Basic(NCSA)",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765049758/LF1-7_essential_wn2r50.png"
+      },
+      {
+        text: "NCSA Boot Camp: รองชนะเลิศอันดับ 2 การแข่งขัน Mini CTF ระดับภูมิภาค",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765049955/messageImage_1765049932230_obyioh.jpg"
+      },
+      {
+        text: "CoE@PSU CTF: รางวัลชมเชย ทีม SirHackalot LKS และอันดับ 2 จากผู้เข้าแข่งขันทั้งหมด 44 คน",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765050005/image_14_w8so9f.png"
+      },
+      {
+        text: "NSC 2025: ผ่านเข้ารอบนำเสนอผลงาน โครงงาน 'LksBrightSteps', React Native + PHP + MySQL",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765050052/LINE_ALBUM_%E0%B8%99%E0%B8%B3%E0%B9%80%E0%B8%AA%E0%B8%99%E0%B8%AD%E0%B9%82%E0%B8%84%E0%B8%A3%E0%B8%87%E0%B8%87%E0%B8%B2%E0%B8%99_NSC_2025_251207_1_fh8hmx.jpg"
+      },
+      {
+        text: "SMTE Northern 2025: เหรียญทอง การนำเสนอโครงงานวิชาการ",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765050320/tt_sqpsfi.png"
+      },
+      {
+        text: "Thailand Cyber Top Talent 2025: ผลการแข่งขันทีมของผมได้ลำดับที่ 66 จากผู้เข้าร่วมแข่งขันทั้งหมด 518 ทีม และผมได้อันดับ 56 ซึ่งเป็นคะแนนเดี่ยวจากผู้แข่งขันทั้งหมด 1319 คน",
+        image: "https://res.cloudinary.com/dg0uswwo4/image/upload/f_auto/v1765050544/image_15_anoxfh.png"
+      },
     ]
   }
-  
-
 ];
